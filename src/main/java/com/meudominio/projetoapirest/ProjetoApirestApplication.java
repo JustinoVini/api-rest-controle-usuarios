83a2a7a2e2d4cd1f249d8ad11aaac9aaf0ac4c1a
Vinicius Justino <visilva5690@hotmail.com>
2023-01-19 20:41:36 -0300

    begin

---
+++ src/main/java/com/meudominio/projetoapirest/ProjetoApirestApplication.java
@@ -0,0 +1,13 @@
+package com.meudominio.projetoapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meudominio/projetoapirest/ProjetoApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meudominio.projetoapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
